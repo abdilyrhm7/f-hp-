f1e8d99c40efd5ba90624c73ee2ff093c265d4f7
abdilyrhm7 <60178586+abdilyrhm7@users.noreply.github.com>
2020-01-29 15:16:04 +0300

    عبدالرحيم العنمي
    بداية كتابة الاكواد للمشروع

---
+++ _____.c
@@ -0,0 +1,26 @@
+  
+    void main() {
+//4×4 keypad
+#define KP_R1   RC4 // ROW 1
+
+ #define KP_R2   RC5 // ROW 2
+ 
+    #define KP_R3   RC6 // ROW 3
+    
+    #define KP_R4  RC7 // ROW 4
+    
+    
+    
+ #define KP_c1   RC3 // colomn 1
+
+ #define KP_c2   RC2 //      colomn 2
+
+    #define KP_c3   RC1 //     colomn 3
+
+    #define KP_c4  RC0 //   colomn 4
+
+
+
+
+  }
+
